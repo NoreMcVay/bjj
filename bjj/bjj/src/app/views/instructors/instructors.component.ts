19746d5c7767c10a94a1003711905c6e2c71e362
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-04-09 12:54:17 +0100

    Update instructors.component.ts

--- bjj/bjj/src/app/views/instructors/instructors.component.ts
+++ bjj/bjj/src/app/views/instructors/instructors.component.ts
@@ -15,28 +15,27 @@ export class InstructorsComponent implements OnInit {
   public instructor: any;
   constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit() { // On component initialisation, make a get request to get all fighters details
+  ngOnInit() { 
       this.http.get('/api/instructors').subscribe(instructors => {
       this.instructors = instructors;
       console.log('GET INSTRUCTORS: ', this.instructors);
     });
   }
 
-  // tslint:disable-next-line:max-line-length
-  getInstructorToEdit(index) {  // Notice on Line 6 of the html file,let i = index and then we pass i to the function. This is the index number of the fighter in the fighters array so we can locate the selected fighter
+  getInstructorToEdit(index) {  
       console.log('button works');
       this.instructor = this.instructors[index];
-      this.router.navigate(['/edit-instructors', this.instructor._id]); // navigate the browser to /edit-fighters/this.fighters._id
+      this.router.navigate(['/edit-instructors', this.instructor._id]); 
   }
 
-  getInstructorProfile(index) { // same thing with the index again
+  getInstructorProfile(index) { 
       this.instructor = this.instructors[index];
       this.router.navigate(['/instructor-profile', this.instructor._id]);
   }
 
   deleteInstructor(index) {
       console.log(index);
-      const instructorId = this.instructors[index]._id; // CHANGED TO CONST
+      const instructorId = this.instructors[index]._id; 
       this.http.delete('/api/delete-instructor/' + `${instructorId}`).subscribe((instructors) => {
           this.instructors = instructors;
           console.log('Delete Instructor Worked along with automatic data refresh!');
