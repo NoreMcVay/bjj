8cf312b3c6dcbf6b66a6bebfc185911249d4b2a8
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-04-10 15:00:27 +0100

    Update instructor-profile.component.ts

--- bjj/bjj/src/app/views/instructor-profile/instructor-profile.component.ts
+++ bjj/bjj/src/app/views/instructor-profile/instructor-profile.component.ts
@@ -22,8 +22,7 @@ export class InstructorProfileComponent implements OnInit {
 
   getInstructor(Id) {
       this.http.get('/api/instructor/' + `${Id}`).subscribe((instructor) => {
-        // tslint:disable-next-line:max-line-length
-        this.instructor = instructor[0]; // It returned an array and I want an object so I select the first object in the array by using [0].
+        this.instructor = instructor[0]; 
         console.log('This Instructor :', this.instructor);
       });
   }
