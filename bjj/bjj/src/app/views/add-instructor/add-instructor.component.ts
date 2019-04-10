b95c9d17b7a56913ff49c2c19713939dd662b327
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-04-10 14:57:10 +0100

    Update add-instructor.component.ts

--- bjj/bjj/src/app/views/add-instructor/add-instructor.component.ts
+++ bjj/bjj/src/app/views/add-instructor/add-instructor.component.ts
@@ -16,16 +16,16 @@ export class AddInstructorComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-      this.belts = ['White', 'Blue', 'Purple', 'Brown', 'Black']; // feeds drop down
-      this.stripes = [0, 1, 2, 3, 4]; // feeds drop down
+      this.belts = ['White', 'Blue', 'Purple', 'Brown', 'Black']; 
+      this.stripes = [0, 1, 2, 3, 4]; 
       this.years = ['0-5', '5-10', '10-15', 'over 15 years'];
       this.instructor = new Instructor();
   }
 
-  addNewInstructor(addInstructorForm) { // the parameter is the form from the template
-      console.log('instructorForm: ', addInstructorForm.value); // we want the form values
-      this.instructor = addInstructorForm.value; // set this.fighter to the form's values
+  addNewInstructor(addInstructorForm) { 
+      console.log('instructorForm: ', addInstructorForm.value); 
+      this.instructor = addInstructorForm.value;
       this.http.post('/api/add-instructor', this.instructor).subscribe(() => console.log('Successfully posted!'));
-      addInstructorForm.resetForm(); // reset the form afterwards to clean it up
+      addInstructorForm.resetForm();
   }
 }
